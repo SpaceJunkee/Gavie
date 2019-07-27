678b37b0d4326293b253e9b22da6b6c4366b7306
Kevin <ksandy@outlook.ie>
2019-07-27 13:14:49 +0100

    Added a scrollview on Movie/Game list

--- app/src/main/java/com/example/gamielist/GameList.java
+++ app/src/main/java/com/example/gamielist/GameList.java
@@ -3,6 +3,12 @@ package com.example.gamielist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class GameList extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class GameList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_list);
+
+        EditText gameTitleEdit = findViewById(R.id.gameTitleEditText);
+
+        gameTitleEdit.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (i == KeyEvent.KEYCODE_ENTER)){
+                    
+                }
+            }
+        });
+
     }
+
 }
--- app/src/main/java/com/example/gamielist/MovieList.java
+++ app/src/main/java/com/example/gamielist/MovieList.java
@@ -3,6 +3,7 @@ package com.example.gamielist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MovieList extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MovieList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list2);
+
+        EditText movieTitleEdit = findViewById(R.id.movieTitleEditText);
     }
 }
