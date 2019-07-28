69622a3973af99f69e68529b4283e33644bbe05a
Kevin <ksandy@outlook.ie>
2019-07-28 16:17:37 +0100

    Added an Arraylist to hold the value of the editText box for both the movie and game list.
    Removed Scroll view and linearLayout and replaced with a listview, which holds the information of the ArrayList after a user has entered a value in the editText

--- app/src/main/java/com/example/gamielist/GameList.java
+++ app/src/main/java/com/example/gamielist/GameList.java
@@ -2,32 +2,53 @@ package com.example.gamielist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class GameList extends AppCompatActivity {
+
+    ArrayList<String> gameList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_list);
 
-        EditText gameTitleEdit = findViewById(R.id.gameTitleEditText);
+        //variables
+        final EditText gameTitleEdit = findViewById(R.id.gameTitleEditText);
+        final ListView gameLV = findViewById(R.id.gameListView);
 
-        gameTitleEdit.setOnKeyListener(new View.OnKeyListener() {
+        gameTitleEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public boolean onKey(View view, int i, KeyEvent keyEvent) {
-                if((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (i == KeyEvent.KEYCODE_ENTER)){
-                    
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                boolean handled = false;
+
+                if(i == EditorInfo.IME_ACTION_GO){
+                    gameList.add(gameTitleEdit.getText().toString());
+                    handled = true;
                 }
+
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>((getApplicationContext()),
+                        android.R.layout.simple_list_item_1, gameList);
+
+                gameLV.setAdapter(arrayAdapter);
+
+                return handled;
             }
         });
-
     }
-
 }
+
+
--- app/src/main/java/com/example/gamielist/MovieList.java
+++ app/src/main/java/com/example/gamielist/MovieList.java
@@ -3,15 +3,50 @@ package com.example.gamielist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MovieList extends AppCompatActivity {
+
+    ArrayList<String> movieList = new ArrayList<String>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list2);
 
-        EditText movieTitleEdit = findViewById(R.id.movieTitleEditText);
+        //variables
+        final EditText movieTitleEdit = findViewById(R.id.movieTitleEditText);
+        final ListView movieLV = findViewById(R.id.movieListView);
+
+        movieTitleEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                boolean handled = false;
+
+                if(i == EditorInfo.IME_ACTION_GO){
+                    movieList.add(movieTitleEdit.getText().toString());
+                    handled = true;
+                }
+
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>((getApplicationContext()),
+                        android.R.layout.simple_list_item_1, movieList);
+
+                movieLV.setAdapter(arrayAdapter);
+
+                return handled;
+            }
+        });
+
+
     }
 }
